6d37f70dc8d8aedef64f955dffcdbaf6b9a44e92
alexRicc2 <alex.65.santanna@gmail.com>
2023-07-24 17:18:24 -0300

    starting Nextjs SaaS with shadcn

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <main>
+      <p className="text-6xl">Hello ai</p>
+      <Button>Ola</Button>
+    </main>
+  )
+}
